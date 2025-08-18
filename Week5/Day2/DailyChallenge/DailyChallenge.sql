3db8c267be4d70c5740bd4b91a0593408de873d4
camimilli <cami.milli@gmail.com>
2025-08-18 17:52:47 +0300

    Week 5 - Day 2 DailyChallengeupdated file

--- Week5/Day2/DailyChallenge/DailyChallenge.sql
+++ Week5/Day2/DailyChallenge/DailyChallenge.sql
@@ -1,3 +1,6 @@
-SELECT COUNT(*) FROM actors 
+SELECT COUNT(*) FROM actors;
 
-INSERT 
+INSERT INTO actors (first_name, last_name, birthdate, number_oscars)
+VALUES ('Matt', '', '06/05/1961', 2);
+
+
